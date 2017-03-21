86335f11397e0f7f0bc97d6dacf8581921112b4a
Piotr Bródka <piotrbrodka95@gmail.com>
2017-03-21 20:26:52 +0100

    porobione obliczenia

--- wyniki.r
+++ wyniki.r
@@ -24,6 +24,8 @@ x8 <- c(2.1, 4.4, 6.8, 9.1, 11.4, 13.7, 16.0, 18.3, 20.6, 22.9, 25.2, 27.6, 29.8
 x9 <- c(1.0, 3.2, 5.3, 7.6, 9.8, 11.9, 14.1, 16.3, 18.4, 20.6, 22.8, 25.0, 27.2, 29.3, 31.4, 33.6, 35.1, 37.9, 40.1, 42.3, 44.5, 46.6, 48.8)
 
 
+
+
 times1 <- diff(x1)
 times2 <- diff(x2)
 times3 <- diff(x3)
@@ -34,16 +36,19 @@ times7 <- diff(x7)
 times8 <- diff(x8)
 times9 <- diff(x9)
 
+ua1 <- sd(times1)/sqrt(length(times1))
+ua2 <- sd(times2)/sqrt(length(times2))
+ua3 <- sd(times3)/sqrt(length(times3))
+ua4 <- sd(times4)/sqrt(length(times4))
+ua5 <- sd(times5)/sqrt(length(times5))
+ua6 <- sd(times6)/sqrt(length(times6))
+ua7 <- sd(times7)/sqrt(length(times7))
+ua8 <- sd(times8)/sqrt(length(times8))
+ua9 <- sd(times9)/sqrt(length(times9))
 
-times1
-times2
-times3
-times4
-times5
-times6
-times7
-times8
-times9
+ua1; ua2; ua3; ua4; ua5; ua6; ua7; ua8; ua9;
+ub <- sqrt(0.1^2/3 + 0.05^2/3)
+ub
 
 m1 <- mean(times1)
 m2 <- mean(times2)
@@ -55,13 +60,17 @@ m7 <- mean(times7)
 m8 <- mean(times8)
 m9 <- mean(times9)
 
-m1
-m2
-m3
-m4
-m5
-m6
-m7
-m8
-m9
+m1; m2; m3; m4; m5; m6; m7; m8; m9
+
+uc1 <- sqrt(ub1^2 + ua^2)
+uc2 <- sqrt(ub2^2 + ua^2)
+uc3 <- sqrt(ub3^2 + ua^2)
+uc4 <- sqrt(ub4^2 + ua^2)
+uc5 <- sqrt(ub5^2 + ua^2)
+uc6 <- sqrt(ub6^2 + ua^2)
+uc7 <- sqrt(ub7^2 + ua^2)
+uc8 <- sqrt(ub8^2 + ua^2)
+uc9 <- sqrt(ub9^2 + ua^2)
+
+uc1; uc2; uc3; uc4; uc5; uc6; uc7; uc8; uc9
 
