3027e8af24582c4aacda7f74c8fed6ee6290e924
Ivan Rukhavets <Ivan Rukhavets>
2017-03-25 16:26:43 +0100

    done stuff

--- wyniki.r
+++ wyniki.r
@@ -2,7 +2,7 @@
 temp = 21
 
 #wektor z okresami
-T <- c()
+T <- c(2.4*.1, c(4.4, 4.0, 3.6)*.05, c(8.4, 7.8, 7, 6.6, 6.2)*.02)
 
 #wektory kolejnych pomiarów [cm]
 x1 <- c(4.8, 9.0, 13.3, 17.5, 21.8, 26.0, 30.3, 34.5, 38.8, 43.0, 47.3)
@@ -23,54 +23,38 @@ x8 <- c(2.1, 4.4, 6.8, 9.1, 11.4, 13.7, 16.0, 18.3, 20.6, 22.9, 25.2, 27.6, 29.8
 
 x9 <- c(1.0, 3.2, 5.3, 7.6, 9.8, 11.9, 14.1, 16.3, 18.4, 20.6, 22.8, 25.0, 27.2, 29.3, 31.4, 33.6, 35.1, 37.9, 40.1, 42.3, 44.5, 46.6, 48.8)
 
+x <- list(x1,x2,x3,x4,x5,x6,x7,x8,x9)
+times <- lapply(x, diff)
+
+ua <- sapply(times, function(times)sd(times)/sqrt(length(times)))
 
 
-
-times1 <- diff(x1)
-times2 <- diff(x2)
-times3 <- diff(x3)
-times4 <- diff(x4)
-times5 <- diff(x5)
-times6 <- diff(x6)
-times7 <- diff(x7)
-times8 <- diff(x8)
-times9 <- diff(x9)
-
-ua1 <- sd(times1)/sqrt(length(times1))
-ua2 <- sd(times2)/sqrt(length(times2))
-ua3 <- sd(times3)/sqrt(length(times3))
-ua4 <- sd(times4)/sqrt(length(times4))
-ua5 <- sd(times5)/sqrt(length(times5))
-ua6 <- sd(times6)/sqrt(length(times6))
-ua7 <- sd(times7)/sqrt(length(times7))
-ua8 <- sd(times8)/sqrt(length(times8))
-ua9 <- sd(times9)/sqrt(length(times9))
-
-ua1; ua2; ua3; ua4; ua5; ua6; ua7; ua8; ua9;
 ub <- sqrt(0.1^2/3 + 0.05^2/3)
 ub
 
-m1 <- mean(times1)
-m2 <- mean(times2)
-m3 <- mean(times3)
-m4 <- mean(times4)
-m5 <- mean(times5)
-m6 <- mean(times6)
-m7 <- mean(times7)
-m8 <- mean(times8)
-m9 <- mean(times9)
+m <- sapply(times, mean)
 
-m1; m2; m3; m4; m5; m6; m7; m8; m9
+uc <- sqrt(ub^2+ua^2)
 
-uc1 <- sqrt(ub1^2 + ua^2)
-uc2 <- sqrt(ub2^2 + ua^2)
-uc3 <- sqrt(ub3^2 + ua^2)
-uc4 <- sqrt(ub4^2 + ua^2)
-uc5 <- sqrt(ub5^2 + ua^2)
-uc6 <- sqrt(ub6^2 + ua^2)
-uc7 <- sqrt(ub7^2 + ua^2)
-uc8 <- sqrt(ub8^2 + ua^2)
-uc9 <- sqrt(ub9^2 + ua^2)
+m
+lambda <- 2*m/100
 
-uc1; uc2; uc3; uc4; uc5; uc6; uc7; uc8; uc9
+f <- c(4167,4545,5000,5556,5952,6410,7143,7576,8065)
+kappa <- (4.81e-26/1.3806e-23)*(lambda^2*f^2)/294.15
+kappa
 
+k=mean(kappa)
+
+dk = sqrt(sum((kappa-k)^2)/9/8)
+
+
+
+dT=c(.2*.1, .2*.05, .2*.05, .2*.05, .2*.02,
+     .2*.02, .2*.02, .2*.02, .2*.02)
+dTe=c(.1*.1, rep(.1*.05, 3), rep(.1*.02, 5))
+ut=sqrt(dT^2/3+dTe^2/3)
+ubf = 1/(T^2)*ut
+
+
+ubkappa <-  (4.81e-26/1.3806e-23/294.15)*sqrt((lambda*f^2)^2*uc^2+(lambda^2*f)^2*ubf^2)
+ubkappa
